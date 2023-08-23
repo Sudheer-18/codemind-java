a9c418ab3007d167c3c1470abcd2cdfe184ba6f7
Sudheer-18 <119719131+Sudheer-18@users.noreply.github.com>
2023-08-23 22:00:31 +0530

    import java.util.*; public class primepallindrome {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         for(int i=n+1;;i++)         {             if(ispallindrome(i))             {                 System.out.println(i);                 break;             }         }     }     public static boolean ispallindrome(int n)     {         int r,s=0,temp=n;         while(n!=0)         {             r=n%10;             s=s*10+r;             n=n/10;         }         if(s==temp)         {             if(isprime(s))             {                 return true;             }         }         return false;     }     public static boolean isprime(int s)     {         int c=0;         for(int i=1;i<=s;i++)         {             if(s%i==0)             {                 c++;             }         }         if(c==2)         {             return true;         }      return  false;     } }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,43 +1,51 @@
 import java.util.*;
-
-public class nextprimepallindrome {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = n + 1; i > 0; i++) {
-            if (ispallindrome(i)) {
+public class primepallindrome
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n+1;;i++)
+        {
+            if(ispallindrome(i))
+            {
                 System.out.println(i);
                 break;
             }
         }
     }
-
-    public static boolean ispallindrome(int n) {
-        int m = n;
-        int r, s = 0;
-        while (n != 0) {
-            r = n % 10;
-            s = s * 10 + r;
-            n = n / 10;
+    public static boolean ispallindrome(int n)
+    {
+        int r,s=0,temp=n;
+        while(n!=0)
+        {
+            r=n%10;
+            s=s*10+r;
+            n=n/10;
         }
-        if (s == m) {
-            if (isprime(s)) {
+        if(s==temp)
+        {
+            if(isprime(s))
+            {
                 return true;
             }
         }
         return false;
     }
-
-    public static boolean isprime(int n) {
-        int c = 0, i;
-        for (i = 1; i <= n; i++) {
-            if (n % i == 0) {
+    public static boolean isprime(int s)
+    {
+        int c=0;
+        for(int i=1;i<=s;i++)
+        { 
+            if(s%i==0)
+            {
                 c++;
             }
         }
-        if (c == 2) {
+        if(c==2)
+        {
             return true;
         }
-        return false;
+     return  false;
     }
 }
